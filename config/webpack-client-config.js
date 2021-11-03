f48453eddf76de170780a68b9c537e10fdc62b7a
brettg94 <brettg94dev@outlook.com>
2021-11-03 15:52:25 -0400

    Add basic react app infrastructure

---
+++ config/webpack-client-config.js
@@ -0,0 +1,110 @@
+require('dotenv').config()
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BUILD_DIR = path.resolve(__dirname, '../dist/public')
+const APP_DIR = path.resolve(__dirname, '../client')
+const ROOT_DIR = path.resolve(__dirname, '../')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+const config = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    main: APP_DIR + '/client-entry.tsx'
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === 'production',
+    usedExports: true,
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      chunks: 'async'
+    }
+  },
+  output: {
+    filename: `[name].[contenthash].js`,
+    chunkFilename: '[name]-[contenthash].js',
+    path: BUILD_DIR,
+    sourceMapFilename: '[name].js.map'
+  },
+  devtool: process.env.NODE_ENV !== 'production' ? 'cheap-source-map' : undefined,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `[name].[contenthash].css`,
+      chunkFilename: '[name]-[contenthash].css',
+      ignoreOrder: false
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    extensions: ['.mjs', '.cjs', '.web.js', '.ts', '.tsx', '.js', '.scss'],
+    modules: ['node_modules'],
+    plugins: [],
+    alias: {}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: '[local]-[hash:base64:12]'
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]-[hash:base64:12]',
+                auto: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.ts*/,
+        exclude: /(node_modules|.webpack)/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              configFile: `${ROOT_DIR}/tsconfig.json`
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              prefix: 'img/',
+              name: 'img-[contenthash].[ext]',
+              publicPath: process.env.CDN_URL && process.env.SERVE_ASSETS_FROM_CDN ? process.env.CDN_URL : '/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
