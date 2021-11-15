0c07ad67cd42f874f5243771245a0c0d2a35d486
brettg94 <brettg94dev@outlook.com>
2021-11-15 12:27:58 -0500

    Better handle malformed tooltips in preview environment

--- server/integrations/contentful/converter/contentful-skill-converter/contentful-skill-converter.ts
+++ server/integrations/contentful/converter/contentful-skill-converter/contentful-skill-converter.ts
@@ -11,6 +11,6 @@ export function convertSkill(entry: Entry<ContentfulSkill>): Skill {
   return {
     title: entry.fields.title,
     //No need to pass a ton of tooltip data over the wire. Instead, pass down the identifier and the client's tooltip system can request them individually if needed.
-    tooltipIdentifier: entry.fields.tooltip ? entry.fields.tooltip.fields.identifier : undefined
+    tooltipIdentifier: entry.fields.tooltip?.fields?.identifier
   }
 }
