00c14cb5970eb617bcb2ab7fe4640c1974591b95
brettg94 <brettg94dev@outlook.com>
2021-11-15 17:35:28 -0500

    Projects page tooltip, adjust some broken projects styling

--- client/components/projects-page/projects-page-ui.tsx
+++ client/components/projects-page/projects-page-ui.tsx
@@ -3,6 +3,8 @@ import { Project } from '@Server/manager/cms/cms-manager'
 import styles from './projects-page.module.scss'
 import { ProjectBlock } from '../project-block/project-block-ui'
 import Masonry from 'react-masonry-css'
+import { TooltipButton } from '../tooltip-button/tooltip-button-container'
+import { TooltipIdentifier } from '@Server/enum/enum'
 
 type Props = {
   projects: Project[]
@@ -23,8 +25,10 @@ export const ProjectsPageUI = React.memo((props: Props) => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.header}>PROJECTS</h2>
-      <div>
+      <h2 className={styles.header}>
+        PROJECTS <TooltipButton identifier={TooltipIdentifier.PROJECTS_PAGE} margin={{ left: true }} />
+      </h2>
+      <div className={styles.interior}>
         <Masonry breakpointCols={masonryBreakpoints} className={styles.masonryGrid} columnClassName={styles.gridColumn}>
           {sortedProjects}
         </Masonry>
--- server/enum/enum.ts
+++ server/enum/enum.ts
@@ -8,5 +8,6 @@ This is likely a result of how enums are handled under the hood with TypeScript.
 
 export enum TooltipIdentifier {
   COVER_PAGE = 'COVER_PAGE',
-  EXPERIENCE_PAGE = 'EXPERIENCE_PAGE'
+  EXPERIENCE_PAGE = 'EXPERIENCE_PAGE',
+  PROJECTS_PAGE = 'PROJECTS_PAGE'
 }
