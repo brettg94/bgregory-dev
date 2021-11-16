61566c34b90ab8437a9895931bee1b415b67ecd9
brettg94 <brettg94dev@outlook.com>
2021-11-16 15:03:20 -0500

    Get rid of ramda to reduce buncle size

--- client/redux/redux.ts
+++ client/redux/redux.ts
@@ -1,6 +1,5 @@
 import { Reducers } from './actions/actions'
 import { Action } from 'redux'
-import { clone } from 'ramda'
 import { Tooltip } from '@Server/manager/cms/cms-manager'
 
 export type State = {
@@ -26,12 +25,12 @@ export const defaultState: State = {
 
 export const createRootReducer = () => {
   return (state: State = defaultState, action: Action) => {
-    let newState = clone(state)
+    let newState = state
 
     for (let i = 0; i < Reducers.length; i++) {
       newState = Reducers[i](newState, action)
     }
 
-    return newState
+    return { ...newState }
   }
 }
